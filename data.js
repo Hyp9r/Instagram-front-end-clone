67c278d45411e30636773e5c1cc8e304115316a5
Tomislav <haxcontroler@gmail.com>
2020-01-16 22:10:55 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,14 @@
+{
+	user: {
+		userIcon: "myIcon.png",
+		username: "tomislav2807"
+	},
+	image: "image.png",
+	likes: 18,
+	comments: [
+	{commentUser: "This is awesome"},
+	{commentUser: "Holy crap"},
+	{commentUser: "Dude where is that?"},
+	{commentUser: "Ohhh its the west side"}
+	]
+}
